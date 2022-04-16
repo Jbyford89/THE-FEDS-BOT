db193f7aef459fdea76f4c03dfaa35ef83fb0351
Jbyford89 <jbyford89@gmail.com>
2022-04-16 14:19:26 -0700

    updated config file

--- config.py
+++ config.py
@@ -10,16 +10,16 @@ BOT_TOKEN = os.getenv("DISCORD_TOKEN")
 OWNER_ID = os.getenv("OWNER_ID")
 
 # Discord Guild ID
-GUILD_ID = int(os.getenv("GUILD_ID"))
+GUILD_ID = os.getenv("GUILD_ID")
 
 # Discord Channel IDs
 
 # Discord Role IDs
-SCRIPT_KIDDIE_ROLE_ID = int(os.getenv("SCRIPT_KIDDIE_ROLE_ID"))
-OVERLORD_ROLE_ID = int(os.getenv("OVERLORD_ROLE_ID"))
-PYTHON_ROLE_ID = int(os.getenv("PYTHON_ROLE_ID"))
-PYTHON_HELP_ROLE_ID = int(os.getenv("PYTHON_HELP_ROLE_ID"))
-UNASSIGNED_ROLE_ID = int(os.getenv("UNASSIGNED_ROLE_ID"))
+SCRIPT_KIDDIE_ROLE_ID = os.getenv("SCRIPT_KIDDIE_ROLE_ID")
+OVERLORD_ROLE_ID = os.getenv("OVERLORD_ROLE_ID")
+PYTHON_ROLE_ID = os.getenv("PYTHON_ROLE_ID")
+PYTHON_HELP_ROLE_ID = os.getenv("PYTHON_HELP_ROLE_ID")
+UNASSIGNED_ROLE_ID = os.getenv("UNASSIGNED_ROLE_ID")
 
 
 # Discord Message IDs
