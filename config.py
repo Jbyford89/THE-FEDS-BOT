8a5cb35e6e5d192fce3a00fb66f207472843bca0
Jbyford89 <jbyford89@gmail.com>
2022-04-20 09:43:13 -0700

    updated config.py

--- config.py
+++ config.py
@@ -6,20 +6,20 @@ load_dotenv()
 # Bot Setup
 PREFIX = ">"
 BOT_NAME = "The Feds"
-BOT_TOKEN = os.getenv("DISCORD_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID"))
+BOT_TOKEN = os.environ["DISCORD_TOKEN"]
+OWNER_ID = os.environ["OWNER_ID"]
 
 # Discord Guild ID
-GUILD_ID = int(os.getenv("GUILD_ID"))
+GUILD_ID = os.environ["GUILD_ID"]
 
 # Discord Channel IDs
 
 # Discord Role IDs
-SCRIPT_KIDDIE_ROLE_ID = int(os.getenv("SCRIPT_KIDDIE_ROLE_ID"))
-OVERLORD_ROLE_ID = int(os.getenv("OVERLORD_ROLE_ID"))
-PYTHON_ROLE_ID = int(os.getenv("PYTHON_ROLE_ID"))
-PYTHON_HELP_ROLE_ID = int(os.getenv("PYTHON_HELP_ROLE_ID"))
-UNASSIGNED_ROLE_ID = int(os.getenv("UNASSIGNED_ROLE_ID"))
+SCRIPT_KIDDIE_ROLE_ID = os.environ["SCRIPT_KIDDIE_ROLE_ID"]
+OVERLORD_ROLE_ID = os.environ["OVERLORD_ROLE_ID"]
+PYTHON_ROLE_ID = os.environ["PYTHON_ROLE_ID"]
+PYTHON_HELP_ROLE_ID = os.environ["PYTHON_HELP_ROLE_ID"]
+UNASSIGNED_ROLE_ID = os.environ["UNASSIGNED_ROLE_ID"]
 
 
 # Discord Message IDs
