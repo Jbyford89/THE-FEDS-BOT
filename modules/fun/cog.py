03075ac3aa52e464f09b73e6e41f41870f55b74e
Jbyford89 <jbyford89@gmail.com>
2022-05-10 18:33:47 -0700

    updated Fun cog added guessing game

--- modules/fun/cog.py
+++ modules/fun/cog.py
@@ -1,5 +1,6 @@
 import random
 from nextcord.ext import commands
+import asyncio
 
 class Fun(commands.Cog, name="Fun"):
     def __init__(self, bot: commands.bot):
@@ -37,6 +38,25 @@ class Fun(commands.Cog, name="Fun"):
             "Very doubtful."
         ]
         await ctx.send(f"🎱 {ctx.author.mention} asked: {question}\n🎱 {random.choice(responses)}")
+
+        @commands.command()
+        async def guess(ctx):
+            await ctx.send('Guess a number between 1 and 10.')
+
+            def is_correct(m):
+                return m.author == ctx.author and m.content.isdigit()
+
+            answer = random.randint(1, 10)
+
+            try:
+                guess = await self.bot.wait_for('message', check=is_correct, timeout=5.0)
+            except asyncio.TimeoutError:
+                return await ctx.send('Sorry, you took too long it was {answer}.')
+
+            if int(guess.content) == answer:
+                await ctx.send('You are correct!')
+            else:
+                await ctx.send(f'Oops, it was {answer}.')
 
 def setup(bot):
     bot.add_cog(Fun(bot))
