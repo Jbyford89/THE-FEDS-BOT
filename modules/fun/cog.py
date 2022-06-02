0f7a92c3124419e453d9a39bd0cf9aa35dca931c
Jbyford89 <jbyford89@gmail.com>
2022-06-02 08:59:32 -0700

    removed duplicate guess command

--- modules/fun/cog.py
+++ modules/fun/cog.py
@@ -53,25 +53,7 @@ class Fun(commands.Cog, name="Fun"):
             raise error
 
 
-
-    @commands.command()
-    async def guess(self, ctx: commands.Context):
-        await ctx.send('Guess a number between 1 and 10.')
-
-        def is_correct(m):
-            return m.author == ctx.author and m.content.isdigit()
-
-        answer = random.randint(1, 10)
-
-        try:
-            guess = await self.bot.wait_for('message', check=is_correct, timeout=5.0)
-        except asyncio.TimeoutError:
-            return await ctx.send('Sorry, you took too long it was {answer}.')
-
-        if int(guess.content) == answer:
-            await ctx.send('You are correct!')
-        else:
-            await ctx.send(f'Oops, it was {answer}.')
+    # Linux jokes
 
 
     @commands.command(name='linuxjoke', description='Get a random joke from the Linux Joke Database', aliases=['lj'])
